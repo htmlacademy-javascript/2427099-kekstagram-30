6a09ac92a83653719a5ed639501f04e8c920cd36
StasVenger <stasvenger1@gmail.com>
2023-10-12 18:32:23 +0600

    Removed comments

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,9 @@ function checkStringLength(string, count) {
   return string.length <= count;
 }
 
-// Cтрока короче 20 символов
-checkStringLength('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-checkStringLength('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-checkStringLength('проверяемая строка', 10); // false
+checkStringLength('проверяемая строка', 20);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 10);
 
 function checkIsPalindrome(string) {
   const updatedString = string.toLowerCase().replaceAll(' ', '');
@@ -20,14 +17,10 @@ function checkIsPalindrome(string) {
   return updatedString === reversedString;
 }
 
-// Строка является палиндромом
-checkIsPalindrome('топот'); // true
-// Несмотря на разный регистр, тоже палиндром
-checkIsPalindrome('ДовОд'); // true
-// Это не палиндром
-checkIsPalindrome('Кекс'); // false
-// Это палиндром
-checkIsPalindrome('Лёша на полке клопа нашёл '); // true
+checkIsPalindrome('топот');
+checkIsPalindrome('ДовОд');
+checkIsPalindrome('Кекс');
+checkIsPalindrome('Лёша на полке клопа нашёл ');
 
 function getIntegerFromString(string) {
   if (typeof string === 'number') {
@@ -43,17 +36,13 @@ function getIntegerFromString(string) {
   }
 
   return parseInt(numberString, 10);
-
-  //Можно сделать одной строкой использую регулярное выражение которое заменит нечисловые значения на пустую строку.
-  //Затем эта строка преобразуется в число.
-  // return parseInt(string.replace(/\D/g, ''), 10);
 }
 
-getIntegerFromString('2023 год'); // 2023
-getIntegerFromString('ECMAScript 2022'); // 2022
-getIntegerFromString('1 кефир, 0.5 батона'); // 105
-getIntegerFromString('агент 007'); // 7
-getIntegerFromString('а я томат'); // NaN
-getIntegerFromString(2023); // 2023
-getIntegerFromString(-1); // 1
-getIntegerFromString(1.5); // 15
+getIntegerFromString('2023 год');
+getIntegerFromString('ECMAScript 2022');
+getIntegerFromString('1 кефир, 0.5 батона');
+getIntegerFromString('агент 007');
+getIntegerFromString('а я томат');
+getIntegerFromString(2023);
+getIntegerFromString(-1);
+getIntegerFromString(1.5);
