987e3ad666d9bc77ff8280beea7dcd4a557b3287
StasVenger <stasvenger1@gmail.com>
2023-10-24 20:46:38 +0600

    Сhanged the names of variables that contain elements

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,20 +1,20 @@
-const picturesList = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesElement = document.querySelector('.pictures');
+const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 
 const pictureListFragment = document.createDocumentFragment();
 
 const showPictures = (pictures) => {
   pictures.forEach(({url, description, likes, comments}) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    const pictureElementImg = pictureElement.querySelector('.picture__img');
-    pictureElementImg.src = url;
-    pictureElementImg.alt = description;
+    const pictureElement = pictureTemplateElement.cloneNode(true);
+    const pictureImgElement = pictureElement.querySelector('.picture__img');
+    pictureImgElement.src = url;
+    pictureImgElement.alt = description;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
     pictureListFragment.appendChild(pictureElement);
   });
 
-  picturesList.appendChild(pictureListFragment);
+  picturesElement.appendChild(pictureListFragment);
 };
 
 export { showPictures };
