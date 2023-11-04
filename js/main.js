40d67344f803b1b7f41a01d4e2d31745c9e8caf6
StasVenger <stasvenger1@gmail.com>
2023-11-04 18:05:04 +0600

    Added module for picture form. Added validation for hashtag and comment fields.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import { getPictures } from './data.js';
 import { showPictures } from './thumbnail.js';
 import { initPictureListeners } from './big-picture.js';
+import { initPictureFormListener } from './picture-form.js';
 
 const pictures = getPictures();
 
 showPictures(pictures);
 
 initPictureListeners(pictures);
+
+initPictureFormListener();
