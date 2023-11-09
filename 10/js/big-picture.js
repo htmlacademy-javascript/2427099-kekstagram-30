a869dc8a855a3fb68203c04102fab3f79597f307
Keks <keks@htmlacademy.ru>
2023-11-09 07:53:40 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/big-picture.js
@@ -0,0 +1,127 @@
+import { isEscapeKey } from './utils.js';
+
+const bodyElement = document.querySelector('body');
+const picturesElement = document.querySelector('.pictures');
+const bigPictureElement = document.querySelector('.big-picture');
+const cancelPictureElement = document.querySelector('#picture-cancel');
+const pictureImgElement = document.querySelector('.big-picture__img img');
+const pictureLikesCountElement = document.querySelector('.likes-count');
+const pictureDescriptionElement = document.querySelector('.social__caption');
+const socialCommentsElement = document.querySelector('.social__comments');
+const commentCountElement = document.querySelector('.social__comment-count');
+const commentsCountTotalElement = commentCountElement.querySelector('.social__comment-total-count');
+const commentsShownElement = commentCountElement.querySelector('.social__comment-shown-count');
+const commentsLoaderElement = document.querySelector('.comments-loader');
+
+const COMMENT_COUNTER = 5;
+
+const createCommentElement = (comment) => {
+  const commentElement = document.createElement('li');
+  commentElement.classList.add('social__comment');
+
+  const avatarImgElement = document.createElement('img');
+  avatarImgElement.classList.add('social__picture');
+  avatarImgElement.src = comment.avatar;
+  avatarImgElement.alt = comment.name;
+  avatarImgElement.width = 35;
+  avatarImgElement.height = 35;
+
+  const socialTextElement = document.createElement('p');
+  socialTextElement.classList.add('social__text');
+  socialTextElement.textContent = comment.message;
+
+  commentElement.appendChild(avatarImgElement);
+  commentElement.appendChild(socialTextElement);
+
+  socialCommentsElement.appendChild(commentElement);
+};
+
+const updateCommentsShownCount = (count) => {
+  commentsShownElement.textContent = count;
+};
+
+const loadComments = (comments) => {
+  let maxComments = comments.length >= COMMENT_COUNTER ? COMMENT_COUNTER : comments.length;
+
+  const loadMoreComments = () => {
+    socialCommentsElement.innerHTML = '';
+    const pictureComments = comments.slice(0, maxComments);
+    pictureComments.forEach((comment) => {
+      createCommentElement(comment);
+    });
+
+    updateCommentsShownCount(maxComments);
+
+    if (comments.length <= maxComments) {
+      commentsLoaderElement.classList.add('hidden');
+    } else {
+      commentsLoaderElement.classList.remove('hidden');
+    }
+  };
+
+  commentsLoaderElement.addEventListener('click', () => {
+    if (maxComments + COMMENT_COUNTER <= comments.length) {
+      maxComments += COMMENT_COUNTER;
+      loadMoreComments();
+    } else {
+      maxComments = comments.length;
+      loadMoreComments();
+    }
+  });
+
+  loadMoreComments();
+};
+
+const renderPictureInfo = (index, pictures) => {
+  pictureImgElement.src = pictures[index].url;
+  pictureLikesCountElement.textContent = pictures[index].likes;
+  pictureDescriptionElement.textContent = pictures[index].description;
+  commentsCountTotalElement.textContent = pictures[index].comments.length;
+
+  loadComments(pictures[index].comments);
+};
+
+const openBigPicture = (index, pictures) => {
+  socialCommentsElement.innerHTML = '';
+  bigPictureElement.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+  bodyElement.classList.add('modal-open');
+
+  renderPictureInfo(index, pictures);
+};
+
+const onPictureClick = (evt, pictures) => {
+  if (evt.target.closest('a.picture')) {
+    const linkElement = evt.target.closest('a.picture');
+    const pictureElements = Array.from(document.querySelectorAll('.picture'));
+    const index = pictureElements.indexOf(linkElement);
+
+    openBigPicture(index, pictures);
+  }
+};
+
+const closeBigPictureModal = () => {
+  bigPictureElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const initPictureListeners = (pictures) => {
+  picturesElement.addEventListener('click', (evt) => {
+    onPictureClick(evt, pictures);
+  });
+
+  cancelPictureElement.addEventListener('click', () => {
+    closeBigPictureModal();
+  });
+};
+
+function onDocumentKeydown (evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPictureModal();
+  }
+}
+
+export { initPictureListeners };
---
+++ 10/js/scale-picture.js
@@ -0,0 +1,32 @@
+const pictureFormElement = document.querySelector('.img-upload__form');
+const scaleControlSmallerElement = pictureFormElement.querySelector('.scale__control--smaller');
+const scaleControlBiggerElement = pictureFormElement.querySelector('.scale__control--bigger');
+const scaleControlValueElement = pictureFormElement.querySelector('.scale__control--value');
+const imagePreviewElement = pictureFormElement.querySelector('.img-upload__preview img');
+
+const SCALE_DEFAULT = 100;
+const SCALE_MIN = 25;
+const SCALE_STEP_COUNT = 25;
+const SCALE_MAX = 100;
+
+const scalePicture = (value) => {
+  scaleControlValueElement.value = `${value}%`;
+  imagePreviewElement.style.transform = `scale(${value / 100})`;
+};
+
+const onSmallerButtonClick = () => {
+  scalePicture(Math.max(parseInt(scaleControlValueElement.value, 10) - SCALE_STEP_COUNT, SCALE_MIN));
+};
+
+const onBiggerButtonClick = () => {
+  scalePicture(Math.min(parseInt(scaleControlValueElement.value, 10) + SCALE_STEP_COUNT, SCALE_MAX));
+};
+
+const initScaleControlListener = () => {
+  scaleControlSmallerElement.addEventListener('click', onSmallerButtonClick);
+  scaleControlBiggerElement.addEventListener('click', onBiggerButtonClick);
+};
+
+const resetScale = () => scalePicture(SCALE_DEFAULT);
+
+export { initScaleControlListener, resetScale };
