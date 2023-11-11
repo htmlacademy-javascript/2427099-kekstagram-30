0ebfd426fea146ce64eb417d20ab56ef6e4f3160
StasVenger <stasvenger1@gmail.com>
2023-11-12 01:47:07 +0600

    Created requests for get and send data. Created functions for show success and error message.

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,3 @@
-const createIdGenerator = () => {
-  let generatorId = 0;
-  return function() {
-    return ++generatorId;
-  };
-};
-
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export { createIdGenerator, getRandomInteger, getRandomArrayElement, isEscapeKey };
+export { isEscapeKey };
