d119843773c4f8fdaa170538bfeafa05700ca0d3
Keks <keks@htmlacademy.ru>
2023-11-16 11:17:52 +0300

    :heavy_check_mark: Сборка #13

--- 13/js/filters.js
+++ 13/js/filters.js
@@ -22,15 +22,18 @@ const filters = {
   [FilterType.DISCUSSED]: (pictures) => pictures.slice().sort((item1, item2) => item2.comments.length - item1.comments.length)
 };
 
+const toggleActiveClass = (evt) => {
+  const currentFilterElement = document.querySelector('.img-filters__button--active');
+  currentFilterElement.classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
+};
+
 const setFilter = (evt, filter, pictures) => {
   const filteredPictures = filters[filter](pictures);
   const picturesElement = document.querySelectorAll('.picture');
   picturesElement.forEach((picture) => picture.remove());
   showPictures(filteredPictures);
   initPictureListeners(filteredPictures);
-  const currentFilterElement = document.querySelector('.img-filters__button--active');
-  currentFilterElement.classList.remove('img-filters__button--active');
-  evt.target.classList.add('img-filters__button--active');
 };
 
 const debouncedSetFilters = debounce(setFilter);
@@ -39,12 +42,15 @@ const initFilters = (pictures) => {
   pictureFilterElement.classList.remove('img-filters--inactive');
   filterDefaultElement.addEventListener('click', (evt) => {
     debouncedSetFilters(evt, FilterType.DEFAULT, pictures);
+    toggleActiveClass(evt);
   });
   filterRandomElement.addEventListener('click', (evt) => {
     debouncedSetFilters(evt, FilterType.RANDOM, pictures);
+    toggleActiveClass(evt);
   });
   filterDiscussedElement.addEventListener('click', (evt) => {
     debouncedSetFilters(evt, FilterType.DISCUSSED, pictures);
+    toggleActiveClass(evt);
   });
 };
 
